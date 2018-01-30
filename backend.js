6fa59d2efee93a65833b95c20ddf4fde0b840f71
Seatis <atikezer@gmail.com>
2018-01-30 20:57:34 +0100

    feat(test-exam): first endpoint has been created

--- backend.js
+++ backend.js
@@ -13,7 +13,7 @@ var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'GR18pv',
-  database: 'licenceplate'
+  database: 'clothes'
 });
 
 connection.connect();
@@ -22,5 +22,15 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/warehouse', function(req, res) {
+  var data = [];
+  var queryString = `SELECT * FROM warehouse`;
+  connection.query(queryString, function(err, result) {
+    result.forEach(function(element){
+      data.push({'item_name': element.item_name, 'manufacturer': element.manufacturer, 'category': element.category, 'size': element.size, 'unit_price': element.unit_price});
+    });
+    res.send({'result': 'OK', 'data': data});
+  });
+});
 
 app.listen(4000, () => console.log('Running'));
